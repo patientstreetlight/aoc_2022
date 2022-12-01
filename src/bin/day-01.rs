4463304d7192b7021da0f6be261fc6087c6504f5
Chris Rice <chris@chrisrice.io>
2022-12-01 00:34:52 -0500

    Day 1

--- src/bin/day-01.rs
+++ src/bin/day-01.rs
@@ -1,5 +1,44 @@
 
 fn main() {
     let input = include_str!("../../inputs/day-01.txt");
-    println!("hello day 1!");
+    println!("part1: {}", part1(input));
+    println!("part2: {}", part2(input));
+}
+
+fn part2(input: &str) -> i32 {
+    // sorted lowest to highest
+    let mut top_3 = [0, 0, 0, 0];
+    fn insert(arr: &mut [i32]) {
+        let mut i = 0;
+        while i < arr.len() - 1 && arr[i] > arr[i+1] {
+            arr.swap(i, i+1);
+            i += 1;
+        }
+        arr[0] = 0;
+    }
+    for line in input.lines() {
+        if line.is_empty() {
+            insert(&mut top_3);
+        } else {
+            top_3[0] += line.parse::<i32>().unwrap();
+        }
+    }
+    insert(&mut top_3);
+    top_3.into_iter().sum()
+}
+
+fn part1(input: &str) -> i32 {
+    let mut max_cals = 0;
+    let mut curr_cals = 0;
+    for line in input.lines() {
+        if line.is_empty() {
+            curr_cals = 0;
+        } else {
+            curr_cals += line.parse::<i32>().unwrap();
+            if curr_cals > max_cals {
+                max_cals = curr_cals;
+            }
+        }
+    }
+    max_cals
 }
